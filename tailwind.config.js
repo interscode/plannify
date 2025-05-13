6366cc18afeda3072ea090982d864c3c53cc066b
mikebalderasanchez <mikebalderassanchez@gmail.com>
2025-05-13 15:09:35 -0600

    fix: add shared and features folder to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 /** @type {import('tailwindcss').Config} */
-export const content = ["./app/**/*.{js,jsx,ts,tsx}"];
+export const content = [
+  "./app/**/*.{js,jsx,ts,tsx}",
+  "./shared/**/*.{js,jsx,ts,tsx}",
+  "./features/**/*.{js,jsx,ts,tsx}",
+];
 export const presets = [require("nativewind/preset")];
 export const theme = {
-  extend: {},
+  extend: {
+    colors: {
+      primary: "#000080",
+      light: "#f8f8ff",
+    },
+  },
 };
 export const plugins = [];
